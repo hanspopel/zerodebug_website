fa69bae549f0a83824445643ce910cb5c730f0dc
hanspopel <pascal@zerodebug.com>
2019-12-03 15:00:17 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zerodebug_website/precache-manifest.c0638270b5fbdd38f5a1eebed7014319.js"
+  "/zerodebug_website/precache-manifest.19f2c6b38776abe92abc359cdc100e73.js"
 );
 
 self.addEventListener('message', (event) => {
