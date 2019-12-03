15eb620e1dbe7412202ab824f0e24cd43971ee94
hanspopel <pascal@zerodebug.com>
2019-12-04 00:00:17 +0700

    Updates

---
+++ precache-manifest.ffb0e20578dbf89e3a907498cc13af9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a4fee191e2ec6ce8c676b53fd0f0f7",
+    "url": "/zerodebug_website/index.html"
+  },
+  {
+    "revision": "f75746b02c6aff6048ef",
+    "url": "/zerodebug_website/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "a18e75ec466227cb61fa",
+    "url": "/zerodebug_website/static/css/main.6ad47f7d.chunk.css"
+  },
+  {
+    "revision": "f75746b02c6aff6048ef",
+    "url": "/zerodebug_website/static/js/2.d65552b3.chunk.js"
+  },
+  {
+    "revision": "a18e75ec466227cb61fa",
+    "url": "/zerodebug_website/static/js/main.1820a24d.chunk.js"
+  },
+  {
+    "revision": "7ef306f05faf54e57f1d",
+    "url": "/zerodebug_website/static/js/runtime-main.978c3053.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zerodebug_website/precache-manifest.19f2c6b38776abe92abc359cdc100e73.js"
+  "/zerodebug_website/precache-manifest.ffb0e20578dbf89e3a907498cc13af9b.js"
 );
 
 self.addEventListener('message', (event) => {
